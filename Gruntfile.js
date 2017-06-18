6f049762d6cfd037588abc5ba86a43394ef14bb4
Patrick Gatewood <pg8wood@vt.edu>
2017-06-18 16:52:43 -0400

    Create Netbeans web project

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+/* 
+ * Created by Patrick Gatewood on 2017.06.18  * 
+ * Copyright © 2017 Patrick Gatewood. All rights reserved. * 
+ */
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
