99cb26fd361cef24babe8f5dc31e00be4ca8c499
Patrick Gatewood <pg8wood@vt.edu>
2017-08-03 22:56:18 -0400

    Emulate transparent header
    Add the home image to the header and scroll
    header's gradient to emulate a transparent yet
    "on-top" sticky header that the content scrolls
    under

--- public_html/js/header.js
+++ public_html/js/header.js
@@ -1,7 +1,16 @@
+$(window).scroll(function () {
+    // Give header opaque background after scrolling approx. one viewport's height 
+    $("#headerNavbar").css("background",
+            $(window).scrollTop() >= $(window).height() * 0.93 ? "#414770" : "");
+
+    // Scroll header gradient along with #intro gradient 
+    $('#navGradientContainer').css('background-position', '0px -' + $(window).scrollTop() + 'px');
+});
+
 function toggleNavbar() {
     $('#headerNavbar').attr("class", $("#headerNavbar").className === "navbar" ?
             "navbar responsive" :
             "navbar");
-    
+
     window.alert($('#headerNavbar').attr("class"));
 }
